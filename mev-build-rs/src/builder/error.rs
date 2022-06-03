337a1829cd0f20edd0e6c527fd877d537ead9a74
Alex Stokes <r.alex.stokes@gmail.com>
2022-06-03 16:47:45 -0600

    supply relay's validator registrations to the builder

--- mev-build-rs/src/builder/error.rs
+++ mev-build-rs/src/builder/error.rs
@@ -1,8 +1,11 @@
 use crate::types::BidRequest as PayloadRequest;
+use ethereum_consensus::primitives::BlsPublicKey;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum Error {
+    #[error("missing preferences for validator with public key {0}")]
+    MissingPreferences(BlsPublicKey),
     #[error("no payload prepared for request: {0:?}")]
     NoPayloadPrepared(PayloadRequest),
 }
