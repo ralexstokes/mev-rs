ce4fdb1da2edcdb24172cded7da5f3aaa43f1cd6
Alex Stokes <r.alex.stokes@gmail.com>
2022-09-07 10:53:38 -0600

    add support for multiple networks

--- mev-rs/src/boost.rs
+++ mev-rs/src/boost.rs
@@ -2,6 +2,7 @@ use crate::config::Config;
 use anyhow::{anyhow, Result};
 use clap::Args;
 use mev_boost_rs::Service;
+use mev_build_rs::Network;
 
 #[derive(Debug, Args)]
 #[clap(about = "🚀 connecting proposers to the external builder network")]
@@ -11,13 +12,13 @@ pub(crate) struct Command {
 }
 
 impl Command {
-    pub(crate) async fn execute(&self) -> Result<()> {
+    pub(crate) async fn execute(&self, network: Network) -> Result<()> {
         let config_file = &self.config_file;
 
         let config = Config::from_toml_file(config_file)?;
 
         if let Some(config) = config.boost {
-            Service::from(config).run().await;
+            Service::from(config, network).run().await;
             Ok(())
         } else {
             Err(anyhow!("missing boost config from file provided"))
