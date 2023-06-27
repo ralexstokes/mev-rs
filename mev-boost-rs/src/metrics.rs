abee0ef092337f9065a8295b1d5ab239a95f8c54
jacobkaufmann <jacobkaufmann18@gmail.com>
2023-06-27 16:10:04 -0600

    refactor: replace lazy_static with OnceLock

--- mev-boost-rs/src/metrics.rs
+++ mev-boost-rs/src/metrics.rs
@@ -1,7 +1,6 @@
-use std::ops::Deref;
+use std::sync::{Once, OnceLock};
 
 use ethereum_consensus::primitives::BlsPublicKey;
-use lazy_static::lazy_static;
 use prometheus::{
     register_histogram_vec, register_int_counter_vec, HistogramOpts, HistogramVec, IntCounterVec,
     Opts, DEFAULT_BUCKETS,
@@ -13,65 +12,96 @@ const SUBSYSTEM: &str = "builder";
 const API_METHOD_LABEL: &str = "method";
 const RELAY_LABEL: &str = "relay";
 
-lazy_static! {
-    pub static ref API_REQUESTS_COUNTER: IntCounterVec = register_int_counter_vec!(
-        Opts::new("api_requests_total", "total number of builder API requests")
-            .namespace(NAMESPACE)
-            .subsystem(SUBSYSTEM),
-        &[API_METHOD_LABEL, RELAY_LABEL]
-    )
-    .unwrap();
-    pub static ref API_TIMEOUT_COUNTER: IntCounterVec = register_int_counter_vec!(
-        Opts::new("api_timeouts_total", "total number of builder API timeouts")
-            .namespace(NAMESPACE)
-            .subsystem(SUBSYSTEM),
-        &[API_METHOD_LABEL, RELAY_LABEL]
-    )
-    .unwrap();
-    pub static ref API_REQUEST_DURATION_SECONDS: HistogramVec = register_histogram_vec!(
-        HistogramOpts {
-            common_opts: Opts::new(
-                "api_request_duration_seconds",
-                "duration (in seconds) of builder API timeouts"
+pub static API_REQUESTS_COUNTER: OnceLock<IntCounterVec> = OnceLock::new();
+pub static API_TIMEOUT_COUNTER: OnceLock<IntCounterVec> = OnceLock::new();
+pub static API_REQUEST_DURATION_SECONDS: OnceLock<HistogramVec> = OnceLock::new();
+
+pub static AUCTION_INVALID_BIDS_COUNTER: OnceLock<IntCounterVec> = OnceLock::new();
+
+static INIT: Once = Once::new();
+
+pub(crate) fn init() {
+    INIT.call_once(|| {
+        API_REQUESTS_COUNTER
+            .set(
+                register_int_counter_vec!(
+                    Opts::new("api_requests_total", "total number of builder API requests")
+                        .namespace(NAMESPACE)
+                        .subsystem(SUBSYSTEM),
+                    &[API_METHOD_LABEL, RELAY_LABEL]
+                )
+                .unwrap(),
             )
-            .namespace(NAMESPACE)
-            .subsystem(SUBSYSTEM),
-            buckets: DEFAULT_BUCKETS.to_vec(),
-        },
-        &[API_METHOD_LABEL, RELAY_LABEL]
-    )
-    .unwrap();
-    pub static ref AUCTION_INVALID_BIDS_COUNTER: IntCounterVec = register_int_counter_vec!(
-        Opts::new("auction_invalid_bids_total", "total number of invalid builder bids")
-            .namespace(NAMESPACE)
-            .subsystem(SUBSYSTEM),
-        &[RELAY_LABEL]
-    )
-    .unwrap();
+            .unwrap();
+
+        API_TIMEOUT_COUNTER
+            .set(
+                register_int_counter_vec!(
+                    Opts::new("api_timeouts_total", "total number of builder API timeouts")
+                        .namespace(NAMESPACE)
+                        .subsystem(SUBSYSTEM),
+                    &[API_METHOD_LABEL, RELAY_LABEL]
+                )
+                .unwrap(),
+            )
+            .unwrap();
+        API_REQUEST_DURATION_SECONDS
+            .set(
+                register_histogram_vec!(
+                    HistogramOpts {
+                        common_opts: Opts::new(
+                            "api_request_duration_seconds",
+                            "duration (in seconds) of builder API timeouts"
+                        )
+                        .namespace(NAMESPACE)
+                        .subsystem(SUBSYSTEM),
+                        buckets: DEFAULT_BUCKETS.to_vec(),
+                    },
+                    &[API_METHOD_LABEL, RELAY_LABEL]
+                )
+                .unwrap(),
+            )
+            .unwrap();
+
+        AUCTION_INVALID_BIDS_COUNTER
+            .set(
+                register_int_counter_vec!(
+                    Opts::new("auction_invalid_bids_total", "total number of invalid builder bids")
+                        .namespace(NAMESPACE)
+                        .subsystem(SUBSYSTEM),
+                    &[RELAY_LABEL]
+                )
+                .unwrap(),
+            )
+            .unwrap();
+    });
 }
 
-pub fn inc_api_int_counter_vec<C: Deref<Target = IntCounterVec>>(
-    counter_vec: &C,
+pub fn inc_api_int_counter_vec(
+    counter_vec: &OnceLock<IntCounterVec>,
     meth: ApiMethod,
     relay: &BlsPublicKey,
 ) {
-    counter_vec.with_label_values(&[meth.as_str(), &relay.to_string()]).inc();
+    if let Some(counter) = counter_vec.get() {
+        counter.with_label_values(&[meth.as_str(), &relay.to_string()]).inc();
+    }
 }
 
-pub fn observe_api_histogram_vec<H: Deref<Target = HistogramVec>>(
-    hist_vec: &H,
+pub fn observe_api_histogram_vec(
+    hist_vec: &OnceLock<HistogramVec>,
     meth: ApiMethod,
     relay: &BlsPublicKey,
     obs: f64,
 ) {
-    hist_vec.with_label_values(&[meth.as_str(), &relay.to_string()]).observe(obs);
+    if let Some(hist) = hist_vec.get() {
+        hist.with_label_values(&[meth.as_str(), &relay.to_string()]).observe(obs);
+    }
 }
 
-pub fn inc_auction_int_counter_vec<C: Deref<Target = IntCounterVec>>(
-    counter_vec: &C,
-    relay: &BlsPublicKey,
-) {
-    counter_vec.with_label_values(&[&relay.to_string()]).inc();
+pub fn inc_auction_int_counter_vec(counter_vec: &OnceLock<IntCounterVec>, relay: &BlsPublicKey) {
+    if let Some(counter) = counter_vec.get() {
+        counter.with_label_values(&[&relay.to_string()]).inc();
+    }
 }
 
 #[derive(Copy, Clone, Debug)]
