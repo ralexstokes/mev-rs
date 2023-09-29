d7881232b32a17b8f36a1b2e90b943deba4c36db
Alex Stokes <r.alex.stokes@gmail.com>
2023-09-29 17:24:59 -0600

    add mev package as overlay and add missing build dep on linxu

--- nix/mev.nix
+++ nix/mev.nix
@@ -4,9 +4,16 @@ let
   commonArgs = {
     pname = "mev-rs";
     src = crane.cleanCargoSource (crane.path ../.);
-    buildInputs = [ ] ++ lib.optionals pkgs.stdenv.isDarwin [
+    buildInputs = lib.optionals pkgs.stdenv.isLinux [
+      openssl
+    ] ++ lib.optionals pkgs.stdenv.isDarwin [
       darwin.apple_sdk.frameworks.Network
     ];
+    nativeBuildInputs = lib.optionals pkgs.stdenv.isLinux [
+      perl
+      clang
+    ];
+    LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
   };
   cargoArtifacts = crane.buildDepsOnly commonArgs;
 in
