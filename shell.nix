5fc3d262857a31bfd49857beba35d7a8df01979d
Alex Stokes <r.alex.stokes@gmail.com>
2023-10-24 17:34:27 -0600

    missing build input to nix shell

--- shell.nix
+++ shell.nix
@@ -6,6 +6,7 @@ mkShell {
   ] ++ lib.optionals pkgs.stdenv.isDarwin [
     libiconv
     darwin.apple_sdk.frameworks.CFNetwork
+    darwin.apple_sdk.frameworks.SystemConfiguration
   ] ++ [
     just
     mdbook
