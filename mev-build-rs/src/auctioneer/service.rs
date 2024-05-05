97aa3b34162e573ea33dc483d2f68b3177012916
gajinder <develop@g11tech.io>
2024-05-05 22:40:58 +0530

    dedup auctioneer config relays

--- mev-build-rs/src/auctioneer/service.rs
+++ mev-build-rs/src/auctioneer/service.rs
@@ -17,7 +17,7 @@ use mev_rs::{
     relay::parse_relay_endpoints,
     signing::sign_builder_message,
     types::{block_submission, BidTrace, SignedBidSubmission},
-    BlindedBlockRelayer, Relay,
+    BlindedBlockRelayer, Relay, RelayEndpoint,
 };
 use reth::{
     api::{EngineTypes, PayloadBuilderAttributes},
@@ -155,8 +155,13 @@ impl<
         context: Arc<Context>,
         genesis_time: u64,
     ) -> Self {
-        let relays =
-            parse_relay_endpoints(&config.relays).into_iter().map(Relay::from).collect::<Vec<_>>();
+        let relays = parse_relay_endpoints(&config.relays);
+        let mut relayset: HashSet<RelayEndpoint> = HashSet::new();
+        for relay in relays.into_iter(){
+          relayset.insert(relay);
+        }
+
+        let relays = relayset.into_iter().map(Relay::from).collect::<Vec<_>>();
 
         config.public_key = config.secret_key.public_key();
 
