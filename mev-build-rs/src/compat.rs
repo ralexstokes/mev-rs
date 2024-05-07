38b2f31b84f7d55f1988ff5d59db499f83263eec
Alex Stokes <r.alex.stokes@gmail.com>
2024-05-07 08:13:19 -0600

    builder: support multiple forks when making block submissions

--- mev-build-rs/src/compat.rs
+++ mev-build-rs/src/compat.rs
@@ -3,14 +3,15 @@ use ethereum_consensus::{
     crypto::{KzgCommitment, KzgProof},
     primitives::{Bytes32, ExecutionAddress},
     ssz::prelude::{ByteList, ByteVector, SimpleSerializeError, U256},
+    Error as ConsensusError, Fork,
 };
 use mev_rs::types::{BlobsBundle, ExecutionPayload};
 use reth::primitives::{Address, BlobTransactionSidecar, Bloom, SealedBlock, B256};
 
 #[cfg(not(feature = "minimal-preset"))]
-use ethereum_consensus::deneb::mainnet as spec;
+use ethereum_consensus::deneb::mainnet as deneb;
 #[cfg(feature = "minimal-preset")]
-use ethereum_consensus::deneb::minimal as spec;
+use ethereum_consensus::deneb::minimal as deneb;
 
 pub fn to_bytes32(value: B256) -> Bytes32 {
     Bytes32::try_from(value.as_ref()).unwrap()
@@ -24,48 +25,52 @@ fn to_byte_vector(value: Bloom) -> ByteVector<256> {
     ByteVector::<256>::try_from(value.as_ref()).unwrap()
 }
 
-// TODO: support multiple forks
-pub fn to_execution_payload(value: &SealedBlock) -> ExecutionPayload {
+pub fn to_execution_payload(value: &SealedBlock, fork: Fork) -> Result<ExecutionPayload, Error> {
     let hash = value.hash();
     let header = &value.header;
     let transactions = &value.body;
     let withdrawals = &value.withdrawals;
-    let transactions = transactions
-        .iter()
-        .map(|t| spec::Transaction::try_from(t.envelope_encoded().as_ref()).unwrap())
-        .collect::<Vec<_>>();
-    let withdrawals = withdrawals
-        .as_ref()
-        .unwrap()
-        .iter()
-        .map(|w| spec::Withdrawal {
-            index: w.index as usize,
-            validator_index: w.validator_index as usize,
-            address: to_bytes20(w.address),
-            amount: w.amount,
-        })
-        .collect::<Vec<_>>();
+    match fork {
+        Fork::Deneb => {
+            let transactions = transactions
+                .iter()
+                .map(|t| deneb::Transaction::try_from(t.envelope_encoded().as_ref()).unwrap())
+                .collect::<Vec<_>>();
+            let withdrawals = withdrawals
+                .as_ref()
+                .unwrap()
+                .iter()
+                .map(|w| deneb::Withdrawal {
+                    index: w.index as usize,
+                    validator_index: w.validator_index as usize,
+                    address: to_bytes20(w.address),
+                    amount: w.amount,
+                })
+                .collect::<Vec<_>>();
 
-    let payload = spec::ExecutionPayload {
-        parent_hash: to_bytes32(header.parent_hash),
-        fee_recipient: to_bytes20(header.beneficiary),
-        state_root: to_bytes32(header.state_root),
-        receipts_root: to_bytes32(header.receipts_root),
-        logs_bloom: to_byte_vector(header.logs_bloom),
-        prev_randao: to_bytes32(header.mix_hash),
-        block_number: header.number,
-        gas_limit: header.gas_limit,
-        gas_used: header.gas_used,
-        timestamp: header.timestamp,
-        extra_data: ByteList::try_from(header.extra_data.as_ref()).unwrap(),
-        base_fee_per_gas: U256::from(header.base_fee_per_gas.unwrap_or_default()),
-        block_hash: to_bytes32(hash),
-        transactions: TryFrom::try_from(transactions).unwrap(),
-        withdrawals: TryFrom::try_from(withdrawals).unwrap(),
-        blob_gas_used: header.blob_gas_used.unwrap(),
-        excess_blob_gas: header.excess_blob_gas.unwrap(),
-    };
-    ExecutionPayload::Deneb(payload)
+            let payload = deneb::ExecutionPayload {
+                parent_hash: to_bytes32(header.parent_hash),
+                fee_recipient: to_bytes20(header.beneficiary),
+                state_root: to_bytes32(header.state_root),
+                receipts_root: to_bytes32(header.receipts_root),
+                logs_bloom: to_byte_vector(header.logs_bloom),
+                prev_randao: to_bytes32(header.mix_hash),
+                block_number: header.number,
+                gas_limit: header.gas_limit,
+                gas_used: header.gas_used,
+                timestamp: header.timestamp,
+                extra_data: ByteList::try_from(header.extra_data.as_ref()).unwrap(),
+                base_fee_per_gas: U256::from(header.base_fee_per_gas.unwrap_or_default()),
+                block_hash: to_bytes32(hash),
+                transactions: TryFrom::try_from(transactions).unwrap(),
+                withdrawals: TryFrom::try_from(withdrawals).unwrap(),
+                blob_gas_used: header.blob_gas_used.unwrap(),
+                excess_blob_gas: header.excess_blob_gas.unwrap(),
+            };
+            Ok(ExecutionPayload::Deneb(payload))
+        }
+        fork => Err(Error::Consensus(ConsensusError::UnsupportedFork(fork))),
+    }
 }
 
 pub fn to_blobs_bundle(sidecars: &[BlobTransactionSidecar]) -> Result<BlobsBundle, Error> {
@@ -83,7 +88,7 @@ pub fn to_blobs_bundle(sidecars: &[BlobTransactionSidecar]) -> Result<BlobsBundl
             proofs.push(proof);
         }
         for blob in &sidecar.blobs {
-            let blob = spec::Blob::try_from(blob.as_ref()).unwrap();
+            let blob = deneb::Blob::try_from(blob.as_ref()).unwrap();
             blobs.push(blob);
         }
     }
